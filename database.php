a546fdcae9361e220baf26d12169096765e72b4e
Gonzalo de la Rosa <gonzalitohj@gmail.com>
2024-06-27 03:19:22 +0200

    Generalización db.php

--- database.php
+++ database.php
@@ -3,12 +3,13 @@
     define("DB_URL", "localhost");
     define("DB_username", "root");
     define("DB_password", "");
+    define("DB_name", "ludiculum");
 
     $db_conn = mysqli_connect(
         DB_URL,
         DB_username,
         DB_password,
-        'ludiculum'
+        DB_name
     ) or die(mysqli_error($mysqli));
 
     mysqli_query($db_conn, "SET NAMES 'utf8'");
